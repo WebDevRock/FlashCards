0f7b54b793212544e8627768d247f94beb4daff9
WebDevRock <chriskirk1806@gmail.com>
2023-10-18 22:23:49 +0100

    filename change in line with project rename

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // App.js
 import React from 'react';
 import './App.css';
-import FlashCard from './FlashCard';
+import FlashCard from './WordWall';
 
 function App() {
   const words = ['Who', 'is', 'it', 'Biff', 'and', 'Chip', 'Mum', 'Kipper', 'Floppy', 'a', 'No', 'it\'s', 'Dad', 'A', 'big', 'little', 'The', 'the', 'Oh']; // Add your words here
